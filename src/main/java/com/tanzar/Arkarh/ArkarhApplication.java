1c21b96cc6d69d7f6fb7c9ba6bd7ee837f812b3d
Tanzar <spakowski19@gmail.com>
2020-01-06 11:49:08 +0100

    Initial build

---
+++ src/main/java/com/tanzar/Arkarh/ArkarhApplication.java
@@ -0,0 +1,13 @@
+package com.tanzar.Arkarh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkarhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArkarhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tanzar/Arkarh/ArkarhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanzar.Arkarh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArkarhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
